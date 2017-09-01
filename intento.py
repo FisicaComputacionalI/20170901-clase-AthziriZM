c7d316e375c325c1966eecc74c8cd6c8f4c64f79
Athziri <31254627+AthziriZM@users.noreply.github.com>
2017-09-01 13:55:32 -0500

    Add files via upload

---
+++ intento.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pylab as pl
+x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+y=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,15,11,6,5,4]
+pl.plot(x, y, 'ro')
+pl.savefig('amigas.png')
