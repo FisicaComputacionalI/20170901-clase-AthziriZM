d1f4a49c76b8d3fa675261a0c737e96dda27d475
Athziri <31254627+AthziriZM@users.noreply.github.com>
2017-09-01 13:12:56 -0500

    Add files via upload

---
+++ tempt.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pylab as pl
+x=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+y=[0,0,1,0,1,1,0,0,1,1,0,0,4,6,8,14,13,10,8,6,5]
+pl.plot(x, y)
+pl.savefig('mascotas.jpg')
